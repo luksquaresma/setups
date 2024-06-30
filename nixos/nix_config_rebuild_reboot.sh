1c2203d03b06fcb48e628497f6bb40bb1b918365
Lucas <lucas.quaresma1@gmail.com>
2024-06-30 07:15:25 -0300

    cachix fix

--- nixos/nix_config_rebuild_reboot.sh
+++ nixos/nix_config_rebuild_reboot.sh
@@ -2,10 +2,10 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 path_checkpoint="$SCRIPTPATH/nix_make_ckeckpoint.sh"
 
-clear;
+clear
 echo "......NIXOS CONFIG-REBUILD-REBOOT SCRIPT......";
 
-sudo -v; cd $SCRIPTPATH; echo;
+sudo -v && cd $SCRIPTPATH && echo
 
 fok() {
     p=$1
@@ -18,35 +18,39 @@ fok() {
 }
 
 {
-    echo
-    echo "...Nix garbage collection..."
-    sudo nix-collect-garbage && echo
+    if read -p "Do you wish to perform garbage collection? (y/N): " && (fok $REPLY); then
+        echo
+        echo "...Nix garbage collection..."
+        sudo nix-collect-garbage && echo
+    fi
 } && {
     echo
-    echo "...Nix cache setup..."
-    sudo cachix use cuda-maintainers && echo
-    sudo cachix use luksquaresma && echo
+    if read -p "Do you wish to use cache via cachix? (y/N): " && (fok $REPLY); then
+        echo;
+        echo "...Nix cache setup..."
+        sudo cachix use luksquaresma && echo
+    fi
 } && {
     echo
     echo "...Nix channel update..."
     sudo nix-channel --update && echo
 } && {
-    echo;
-    echo "...Nix config edit...";
+    echo
+    echo "...Nix config edit..."
     sudo nano /etc/nixos/configuration.nix && echo
 } && {
     if read -p "Do you wish to rebuild on boot? (y/N): " && (fok $REPLY); then
-        echo;
+        echo
         sudo nixos-rebuild boot && echo
     fi
 } && {
     if read -p "Do you wish to push the current cache? (y/N): " && (fok $REPLY); then
         echo;
-        sudo nix-build | cachix push luksquaresma && echo
+        sudo cachix push luksquaresma * && echo
     fi
 } && {
     if read -p "Do you wish to start checkpoint script? (y/N): " && (fok $REPLY); then
-        echo; bash $path_checkpoint; echo;
+        echo && bash $path_checkpoint && echo
     fi
 } && {
     if read -p "Do you wish to restart now? (y/N): " && (fok $REPLY); then
