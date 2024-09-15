8ea59e4289c7307f9dd204cca157598355bf6c4d
Lucas Quaresma <lucas.quaresma1@gmail.com>
2024-09-15 20:09:24 -0300

    Manual NixOS configuration sync via Bash

--- nixos/tmp/configuration.nix
+++ nixos/tmp/configuration.nix
@@ -9,9 +9,9 @@ in {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      
+
       # cache from cachix
-      ./cachix.nix
+      # ./cachix.nix
     ];
 
   # Bootloader.
@@ -55,12 +55,9 @@ in {
 
   # Configure keymap in X11
   services.xserver.xkb = {
-    layout = "br";
-    variant = "thinkpad";
+    layout = "us";
+    variant = "";
   };
-
-  # Configure console keymap
-  console.keyMap = "br-abnt2";
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -77,30 +74,6 @@ in {
   # QMK VIA Suport
   hardware.keyboard.qmk.enable = true;
   services.udev.packages = [ pkgs.via ];
-
-  # ==== TEMPORARY FIX FOR AUDIO BUGS ON KERNEL
-  # boot.extraModprobeConfig = "options snd-hda-intel model=auto";
-  # boot.kernelModules = ["snd_hda_intel"];
-  # boot.kernelParams = [ "snd-intel-dspcfg.dsp_driver=1" ];  
-  # boot.blacklistedKernelModules = [
-  #   "snd_soc_avs"
-  # ];
-  # boot.kernelPatches = [
-  #   {
-  #     name = "fix-audio-1";
-  #     patch = builtins.fetchurl {
-  #       url = "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/patch/sound/soc/soc-topology.c?id=e0e7bc2cbee93778c4ad7d9a792d425ffb5af6f7";
-  #       sha256 = "sha256:1y5nv1vgk73aa9hkjjd94wyd4akf07jv2znhw8jw29rj25dbab0q";
-  #     };
-  #   }
-  #   {
-  #     name = "fix-audio-2";
-  #     patch = builtins.fetchurl {
-  #       url = "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/patch/sound/soc/soc-topology.c?id=0298f51652be47b79780833e0b63194e1231fa34";
-  #       sha256 = "sha256:14xb6nmsyxap899mg9ck65zlbkvhyi8xkq7h8bfrv4052vi414yb";
-  #     };
-  #   }
-  # ];
 
   # Enable sound with pipewire.
   hardware.pulseaudio = {
@@ -149,9 +122,11 @@ in {
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.cudaSupport = true;
 
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    alacarte
     btop
     cachix
     curl
@@ -278,21 +253,11 @@ in {
     open = false;
 
     # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
+        # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  hardware.nvidia.prime = {
-    intelBusId = "PCI:0:2:0";
-    nvidiaBusId = "PCI:1:0:0";
-    sync.enable = true;
-    # offload = {
-    #   enable = true;
-    #   enableOffloadCmd = true;
-    # };
   };
 
   # ===== GARBAGE COLLECTOR
