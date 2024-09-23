8c271556091cf76865e32e66f4c38bc10b3417db
Lucas Quaresma <lucas.quaresma1@gmail.com>
2024-09-23 17:25:15 -0300

    trying to fix bluetooth codecs

--- nixos/tmp/configuration.nix
+++ nixos/tmp/configuration.nix
@@ -63,11 +63,18 @@ in {
   services.printing.enable = true;
 
   hardware.enableRedistributableFirmware = true;
-
+  hardware.enableAllFirmware = true;
+  
   # ===== BLUETOOTH CONFIFGS
   hardware.bluetooth = {
     enable = true; # enables support for Bluetooth
     powerOnBoot = true; # powers up the default Bluetooth controller on boot
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+        Experimental = true;
+      };
+    };
   };
   services.blueman.enable = true;
 
