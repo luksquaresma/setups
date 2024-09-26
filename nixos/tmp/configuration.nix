afd9e74412b17dbdc3184ccddb8235365068cf77
Lucas Quaresma <lucas.quaresma1@gmail.com>
2024-09-26 16:17:41 -0300

    Manual NixOS configuration sync via Bash

--- nixos/tmp/configuration.nix
+++ nixos/tmp/configuration.nix
@@ -158,7 +158,7 @@ in {
     neofetch
     nix-search-cli
     obsidian
-    unstable.ollama
+    unstable.oterm
     pavucontrol
     pciutils
     pulsemixer
@@ -195,6 +195,22 @@ in {
     # python311Packages.tensorflowWithCuda
     # python311Packages.tensorrt
   ];
+
+  # ===== OLLAMA
+  services.ollama = {
+    package = unstable.ollama; # Uncomment if you want to use the unstable channel, see https://fictionbecomesfact.com/nixos-unstable-channel
+    enable = true;
+    acceleration = "cuda"; # Or "rocm"
+    #environmentVariables = { # I haven't been able to get this to work myself yet, but I'm sharing it for the sake of completeness
+      # HOME = "/home/ollama";
+      # OLLAMA_MODELS = "/home/ollama/models";
+      # OLLAMA_HOST = "0.0.0.0:11434"; # Make Ollama accesible outside of localhost
+      # OLLAMA_ORIGINS = "http://localhost:8080,http://192.168.0.10:*"; # Allow access, otherwise Ollama returns 403 forbidden due to CORS
+    #};
+  };
+
+
+
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
