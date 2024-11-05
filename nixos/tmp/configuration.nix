6d1a7a55753684e2a6bfe7f1e26b2db04b2523a4
Lucas Quaresma <lucas.quaresma1@gmail.com>
2024-11-05 16:55:25 -0300

    audio fix working with bluetooth mic

--- nixos/tmp/configuration.nix
+++ nixos/tmp/configuration.nix
@@ -109,27 +109,30 @@ in {
   #   }
   # ];
 
-  # Enable sound with pipewire.
+  # ===== SOUND WITH PIPEWIRE
+  sound.enable = false;
+  security.rtkit.enable = true;
   hardware.pulseaudio = {
     enable = false;
-    package = pkgs.pulseaudioFull;
+    package = unstable.pulseaudioFull;
   };
-  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     jack.enable = true;
-    
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    wireplumber = {
+      enable = true;
+      package = unstable.wireplumber;
+    };    
   };
-  boot.extraModprobeConfig = ''
-    options snd-intel-dspcfg dsp_driver=1
-  '';
+  
+  # Fix for audio testing
+  # boot.extraModprobeConfig = ''
+  #   options snd-intel-dspcfg dsp_driver=1
+  # '';
+
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
 
